78705c89f4cdd18c58820b4ae3d5e5af7772fa78
dragon66 <yuwen_66@yahoo.com>
2022-02-01 11:02:52 -0500

    Create CheckTreeBalance.java

---
+++ src/com/icafe4j/goodies/trees/CheckTreeBalance.java
@@ -0,0 +1,33 @@
+package com.icafe4j.goodies.trees;
+
+public class CheckTreeBalance {
+   
+   public static void main( String[] args) {
+      TreeNode root = new TreeNode();
+      root.left = new TreeNode();
+      root.left.left = new TreeNode();
+      root.right = new TreeNode();
+      System.out.println(depth(root));
+      System.out.println(is balanced(root));
+   }
+
+   public static boolean isBalanced(TreeNode root) {
+      return depth(root) != -1; // -1 means not balanced
+   }
+
+   public static int depth(TreeNode root) {
+      if(root == null)
+         return 0; // see note
+      int lChildH = depth(root.left);
+      if(lChildH == -1) return -1;
+      int rChildH = depth(root.right);
+      if(rChildH == -1) return -1;
+      if(lChildH != rChildH) return -1;
+      return 1 + lChildH; // lChildH == rChildH
+   }
+
+   private static class TreeNode {
+      private TreeNode left;
+      private TreeNode right;
+   }
+}
